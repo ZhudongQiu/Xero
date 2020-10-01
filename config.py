d99260f7ab8411d61fa6a2946545b744431cd81e
ZhudongQiu <d958155725@gmail.com>
2020-09-30 22:12:35 -0700

    config

--- config.py
+++ config.py
@@ -8,5 +8,9 @@ Config = {'Xero_test':'Xero_project/Xero_test.csv',
           'Xero_train':'Xero_project/Xero_train.csv',
           'Xero_features':'Xero_project/Xero_features.csv',
           'Outcome':'Xero_project/Outcome.json',
+          'LinearRegression_forecast':'Xero_project/LinearRegression_forecas.PNG',
+          'LSTM_forecast':'Xero_project/LSTM_forecast.PNG',
+          'RandomForest_forecast':'Xero_project/RandomForest_forecast.PNG',
+          'XGBoost_forecast':'Xero_project/XGBoost_forecast.PNG','
           'RandomForestRegressor_setting':{'n_estimators':10},
           'XGBRegressor_setting':{'n_estimators':100,'learning_rate':0.2}}
